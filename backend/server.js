99fe32e9eacbf4ab68bbf5c81b3482d4dee1e6cb
lotekone <joar@dedypeskoger>
2022-06-16 14:11:01 +0200

    added routing

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
-const express = require("express");
 require("dotenv").config();
+
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 // express app
 const app = express();
 
-//middleware
+// middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get("/", (req, res) => {
-  res.json({ mssg: "Hello World" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 // listen for requests on port 4000
 app.listen(process.env.PORT, () => {
